a3e59179b83ad27944f0a631b87bc8611e4e880e
rebeckhey <rebecka.pettersson.1993@gmail.com>
2021-11-10 15:45:00 +0100

    second

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -8,10 +8,10 @@ const ProductCard = ({product}) => {
 
     return (
       
-        <div className="col" >
+        <div className="d-flex justify-content-start col" >
            
-            <div className="card img-size bord mt-5">
-    <img className="p-3" src={product.productImageLink} alt="christmas wreath"/>
+            <div className="card  bord mt-5">
+    <img className="p-3 img-size" src={product.productImageLink} alt="christmas wreath"/>
     <h1 className="Nav-font smaller d-flex justify-content-center">{product.productName}</h1>
     <div className=" ">
         <div className="d-flex justify-content-center">
@@ -20,7 +20,7 @@ const ProductCard = ({product}) => {
         <div className="d-flex justify-content-center">
     
     <Link to={`products/${product.id}`}>
-    <button className="btn btn-info Nav-font size">View product</button>
+    <button className="btn btn-info Nav-font size mb-5">View product</button>
 
         </Link>
 
